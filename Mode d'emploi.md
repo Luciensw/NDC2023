bb0aa1b267fb72f5597a0f819428032357882ea9
Luciensw <lucien.sevault-wolber@eleves.ecmorlaix.fr>
2023-05-23 22:14:50 +0200

    muisic

---
+++ Mode d'emploi.md
@@ -0,0 +1,4 @@
+# Voici notre jeu : Spartacus bros brawl super ultra ultimate !!!!
+Ce jeu est un smash bros like qui se joue à 2 joueurs.
+Le joueur 1 utilise les touches ZQSD pour se déplacer et la barre espace pour attaquer.
+Le joueur 2 utilise les flèches du clavier pour se déplacer et la touche entrée pour attaquer.
--- ndc.py
+++ ndc.py
@@ -16,7 +16,12 @@ class Jeu():
         self.j2y = 60
         self.jumpj1 = "Ground"
         self.jumpj2 = "Ground"
+        self.j1hp = 5
+        self.j2hp = 5
+        self.j1sword = False
+        self.j2sword = False
         pyxel.load("theme.pyxres")
+        pyxel.music(0)
         pyxel.run(self.update, self.draw)
 
 
@@ -28,12 +33,12 @@ class Jeu():
 
 
         # Déplacement joueur 1
-        j1x = 0
         if pyxel.btn(pyxel.KEY_D):
-            j1x += 1
+            self.j1x += 1
         if pyxel.btn(pyxel.KEY_Q):
-            j1x -= 1
-        self.j1x += j1x
+            self.j1x -= 1
+        if pyxel.btn(pyxel.KEY_SPACE):
+            not self.j1sword
 
 
         # Déplacement joueur 2
@@ -41,6 +46,8 @@ class Jeu():
             self.j2x += 1
         if pyxel.btn(pyxel.KEY_LEFT):
             self.j2x -= 1
+        if pyxel.btn(pyxel.KEY_RETURN):
+            not self.j2sword
 
 
         # Saut et gravité
@@ -70,7 +77,7 @@ class Jeu():
     def draw(self):
         pyxel.cls(0)
         pyxel.blt(self.j1x, self.j1y, 0, 0, 0, 8, 8, 0)
-        pyxel.blt(self.j2x, self.j2y, 0, 0, 0, 8, 8, 0)
+        pyxel.blt(self.j2x, self.j2y, 0, 0, 8, -8, 8, 0)
 
 
 
